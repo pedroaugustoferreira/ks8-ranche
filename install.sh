989e7f24a5d23e04d2ad673f6da472202c302963
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2023-02-19 08:00:59 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+  echo "export IP=$IP"
+  sleep 5;
+
+  curl https://releases.rancher.com/install-docker/19.03.sh | sh
+  usermod -aG docker service
+  apt-get install git -y
+  curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+  ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+cat <<EOF >/etc/network/interfaces
+source /etc/network/interfaces.d/*
+
+auto lo
+iface lo inet loopback
+
+auto enp3s0
+iface enp3s0 inet static
+  address $IP
+  netmask 255.255.255.0
+  gateway 192.168.100.1
+  dns-nameservers 192.168.100.150
+EOF
